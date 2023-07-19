31ba1b550db77c742f8a9c9e54accc5ee1b06547
hipolito <huaylla7@hotmail.com>
2023-07-19 19:35:54 -0400

    Initial commit

---
+++ app/src/main/java/org/demre/recyclerpaiseskotlin/Adapter.kt
@@ -0,0 +1,23 @@
+package org.demre.recyclerpaiseskotlin
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+    class ViewHolder {
+
+    }
+}
